8c9302beb3519ee61f131f9c6ca0bf6fe23ad03d
Bruno Nascimento <Bruno Nascimento@DESKTOP-3M9QPCB>
2025-10-17 20:27:08 -0300

    Add project files.

---
+++ LocadoraCarros/Adress.cs
@@ -0,0 +1,29 @@
+﻿namespace LocadoraCarros;
+public class Adress(string street, int number, string neighborhood, string city, string country)
+{
+    public string Street { get; private set; } = street;
+    public int Number { get; private set; } = number;
+    public string? Complement { get; private set; } = null;
+    public string Neighborhood { get; private set; } = neighborhood;
+    public string City { get; private set; } = city;
+    public string Country { get; private set; } = country;
+
+
+    public Adress(string street, int number, string neighborhood, string city, string country, string complement)
+        : this (street, number, neighborhood, city, country)
+    {
+        Complement = complement;
+    }
+
+    public override string ToString()
+    {
+        return $"\n" +
+            $"Adress\n" +
+            $"Street: {Street}\n" +
+            $"Number: {Number}\n" +
+            $"{(Complement is null ? "" : $"Complement: {Complement}\n")}" +
+            $"Neighborhood: {Neighborhood}\n" +
+            $"City: {City}\n" +
+            $"Country: {Country}\n";
+    }
+}
---
+++ LocadoraCarros/Client.cs
@@ -0,0 +1,20 @@
+﻿namespace LocadoraCarros;
+
+abstract class Client(int id, string name, string surname, DateTime birthDate, Adress adress)
+{
+    public int Id { get; init; } = id;
+    public string Name { get; private set; } = name;
+    public string Surname { get; private set; } = surname;
+    public DateTime BirthDate { get; init; } = birthDate;
+    public Adress Adress { get; private set; } = adress;
+
+    public override string ToString()
+    {
+        return $"\n" +
+            $"Client\n" +
+            $"Name: {Name}\n" +
+            $"Surname: {Surname}\n" +
+            $"Birth Date: {BirthDate}\n" +
+            $"{Adress}";
+    }
+}
