fb6248414c0ccc32d5160803ca502324b89269c7
Bruno Nascimento <Bruno Nascimento@DESKTOP-3M9QPCB>
2025-10-18 16:16:52 -0300

    feat: implment Motocycle and Truck

---
+++ LocadoraCarros/Entities/Motorcycle.cs
@@ -0,0 +1,29 @@
+﻿using LocadoraCarros.Entities.Base;
+using LocadoraCarros.Entities.Enums;
+
+namespace LocadoraCarros.Entities;
+
+internal class Motorcycle : AbstractVehicle
+{
+    public int EngineCylinderCapacityCC { get; private set; }
+    public bool HasStorageBox { get; private set; }
+    public bool IsAutomatic { get; private set; }
+    public Motorcycle(int id, string model, string manufacturer, int manufacturerYear, Color color,
+        decimal dailyRentalPrice, int mileage, VehicleType vehicleType, string licensePlate,
+        int engineCylinderCapacityCC, bool hasStorageBox, bool isAutomatic)
+    : base(id, model, manufacturer, manufacturerYear, color, dailyRentalPrice, mileage, vehicleType, licensePlate)
+    {
+        EngineCylinderCapacityCC = engineCylinderCapacityCC;
+        HasStorageBox = hasStorageBox;
+        IsAutomatic = isAutomatic;
+    }
+
+    public override string ToString()
+    {
+        return $"\nMotocycle\n\r" +
+               $"{base.ToString()}\n\r" +
+               $"Engine cylinder capacity: {EngineCylinderCapacityCC}cc\n\r" +
+               $"{(HasStorageBox ? "Has storage box" : "Don't have storage box")}\n\r" +
+               $"{(IsAutomatic ? "Is automatic" : "Is manual")}\n\r";
+    }
+}
