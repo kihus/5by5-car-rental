3e6c57a063439fcc27f8c842fe69dd6a4b51fd0d
Bruno Nascimento <Bruno Nascimento@DESKTOP-3M9QPCB>
2025-10-19 11:23:44 -0300

    reafactor: `IVehicleRepository`

--- LocadoraCarros/Contracts/IVehicleRepository.cs
+++ LocadoraCarros/Contracts/IVehicleRepository.cs
@@ -1,25 +1,15 @@
-﻿using LocadoraCarros.Entities.Base;
+﻿using LocadoraCarros.Entities;
+using LocadoraCarros.Entities.Base;
 
 namespace LocadoraCarros.Contracts;
 
 internal interface IVehicleRepository
 {
-    public List<AbstractVehicle> Vehicle { get; set; }
-
-    public void AddVehicle(AbstractVehicle vehicle);
+    public void AddMotorcycle(Motorcycle vehicle);
+    public void AddCar(Car vehicle);
+    public void AddTruck(Truck vehicle);
     public void RemoveVehicle(AbstractVehicle vehicle);
-    public List<AbstractVehicle> GetAll()
-    {
-        return Vehicle;
-    }
-
-    public List<AbstractVehicle> GetById(int id)
-    {
-        return Vehicle.Where(x => x.Id == id).ToList();
-    }
-
-    public List<AbstractVehicle> GetByModel(string model)
-    {
-        return Vehicle.Where(x => x.Model == model).ToList();
-    }
+    public void GetAll();
+    public void GetById(int id);
+    public void GetByModel(string model);
 }
---
+++ LocadoraCarros/Services/VehicleRepositoryService.cs
@@ -0,0 +1,37 @@
+﻿using LocadoraCarros.Contracts;
+using LocadoraCarros.Entities;
+using LocadoraCarros.Entities.Base;
+
+namespace LocadoraCarros.Services;
+
+internal class VehicleRepositoryService : IVehicleRepository
+{
+    private List<Car> _cars { get; set; }
+    private List<Motorcycle> _motorcycle { get; set; }
+    private List<Truck> _truck { get; set; }
+
+    public void AddVehicle(AbstractVehicle vehicle)
+    {
+        throw new NotImplementedException();
+    }
+
+    public List<AbstractVehicle> GetAll()
+    {
+        throw new NotImplementedException();
+    }
+
+    public List<AbstractVehicle> GetById(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public List<AbstractVehicle> GetByModel(string model)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void RemoveVehicle(AbstractVehicle vehicle)
+    {
+        throw new NotImplementedException();
+    }
+}
