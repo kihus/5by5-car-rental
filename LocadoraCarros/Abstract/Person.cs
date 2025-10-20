fa51b6e703675daf8e30447142edb2bccea27edd
Bruno Nascimento <Bruno Nascimento@DESKTOP-3M9QPCB>
2025-10-20 10:22:07 -0300

    Add `CustomerPF`

--- LocadoraCarros/Abstract/Person.cs
+++ LocadoraCarros/Abstract/Person.cs
@@ -4,8 +4,26 @@ namespace LocadoraCarros.Abstract;
 
 internal abstract class Person
 {
-    public string Name { get; private set; }
-    public DateTime BirthDate { get; private set; }
-    public Contact Contact { get; private set; }
-    public Adress Adress { get; private set; }
+    private string _name { get; set; }
+    private DateOnly _birthDate { get; set; }
+    private Contact _contact { get; set; }
+    private Adress _adress { get; set; }
+
+    public Person(string name, DateOnly birthDate, Contact contact, Adress adress)
+    {
+        _name = name;
+        _birthDate = birthDate;
+        _contact = contact;
+        _adress = adress;
+    }
+
+    public string GetName()
+    {
+        return _name;
+    }
+
+    public override string ToString()
+    {
+        return $"Name: {_name}, Birth date: {_birthDate}, Contact: {_contact}, Adress: {_adress}";
+    }
 }
---
+++ LocadoraCarros/Models/CustomerPF.cs
@@ -0,0 +1,25 @@
+﻿using LocadoraCarros.Abstract;
+
+namespace LocadoraCarros.Models;
+
+internal class CustomerPF(
+    string name,
+    DateOnly birthDate,
+    Contact contact,
+    Adress adress,
+    string cnh,
+    string cpf
+    )
+    : Person(
+        name,
+        birthDate,
+        contact,
+        adress
+        )
+{
+    private Guid _id { get; set; } = Guid.NewGuid();
+    private string _cnh { get; set; } = cnh;
+    private string _cpf { get; set; } = cpf;
+
+
+}
